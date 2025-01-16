bad937dd61298b7dbc87add1389db0d850604ff6
DavidUrbaez <david.urbaez@smartwires.com>
2025-01-16 17:02:41 -0500

    Updates

---
+++ precache-manifest.0e526664a835e281892a295dd81664bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b578c9ffd27fe128e0495791195189b1",
+    "url": "/csv_loader_react/index.html"
+  },
+  {
+    "revision": "9889e1b091f5489bcd72",
+    "url": "/csv_loader_react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54f4d3849b6b571c7032",
+    "url": "/csv_loader_react/static/js/2.c20ab8ae.chunk.js"
+  },
+  {
+    "revision": "9889e1b091f5489bcd72",
+    "url": "/csv_loader_react/static/js/main.7d5abcc4.chunk.js"
+  },
+  {
+    "revision": "708a6c6276325901b180",
+    "url": "/csv_loader_react/static/js/runtime~main.44c441e2.js"
+  }
+]);
